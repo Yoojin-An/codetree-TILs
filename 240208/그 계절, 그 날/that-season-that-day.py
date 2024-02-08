5d2dea9411bb5b379e41e8ae0999f44c7a1a6726
yoojin <65722972+Yoojin-An@users.noreply.github.com>
2024-02-08 22:40:42 +0900

    Title: 그 계절, 그 날, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240208/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,28 @@
+def is_validate(Y:int, M:int, D: int) -> bool:
+    is_validate = True
+    leap_year = False    
+    if Y % 4 == 0 and Y % 100 != 0:
+        leap_year = True
+    elif Y % 4 == 0 and Y % 400 == 0:
+        leap_year = True
+    if leap_year:
+        if M == 2 and D > 29:
+            is_validate = False
+    else:
+        if M in(2, 4, 6, 9, 11):
+            if D == 31:
+                is_validate = False
+    return is_validate
+
+Y, M, D = map(int, input().split())
+if is_validate(Y, M, D):
+    if M in (3, 4, 5):
+        print("Spring")
+    elif M in (6, 7, 8):
+        print("Summer")
+    elif M in (9, 10, 11):
+        print("Fall")
+    else:
+        print("Winter")
+else:
+    print(-1)
