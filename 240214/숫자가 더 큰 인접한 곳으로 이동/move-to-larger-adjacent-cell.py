eb43048a557a7aae43bc04e78f831bfaf52e5116
yoojin <65722972+Yoojin-An@users.noreply.github.com>
2024-02-14 12:56:25 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240214/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240214/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -5,12 +5,14 @@ def move(grid, n, r, c):
     r -= 1
     c -= 1
     curr_value = grid[r][c]
+    next_value = 0
     answer = [curr_value]
     dxs, dys = [-1, 1, 0, 0], [0, 0, -1, 1]
     for x, y in zip(dxs, dys):
         next_x, next_y = r + x, c + y
-        next_value = grid[next_x][next_y]
-        if in_range(n, next_x, next_y) and next_value > curr_value:
+        if in_range(n, next_x, next_y):
+            next_value = grid[next_x][next_y]
+        if next_value > curr_value:
             answer.append(next_value)
             curr_value = next_value
             r, c = next_x, next_y
