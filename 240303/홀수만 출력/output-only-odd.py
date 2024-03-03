5d091b0cc9c83d53d973f1595cee8f2e102d7945
yoojin <65722972+Yoojin-An@users.noreply.github.com>
2024-03-03 12:14:05 +0900

    Title: 홀수만 출력, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240303/홀수만 출력/output-only-odd.py
+++ 240303/홀수만 출력/output-only-odd.py
@@ -1,8 +1,3 @@
 a, b = map(int, input().split())
-arr = []
-for i in range(a, b + 1):
-    if i % 2 != 0:
-        arr.append(i)
-arr.sort()
-for i in arr:
+for i in range(a, b + 1, 2):
     print(i, end = " ")
