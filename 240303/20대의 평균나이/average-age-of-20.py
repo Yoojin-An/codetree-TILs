700b04ca1e169588a2882058d31822aa525d7633
yoojin <65722972+Yoojin-An@users.noreply.github.com>
2024-03-03 14:03:02 +0900

    Title: 20대의 평균나이, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240303/20대의 평균나이/average-age-of-20.py
+++ 240303/20대의 평균나이/average-age-of-20.py
@@ -1,8 +1,8 @@
 arr = []
 while True:
     age = int(input())
-    if age < 20 or age > 30:
-        break
+    if not 20 <= age < 30:
+      break
     arr.append(age)
 
 print("{:.2f}".format(sum(arr)/len(arr)))
