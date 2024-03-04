60a1d944a812a5af656350c419e8b0c1638b7bc1
yoojin <65722972+Yoojin-An@users.noreply.github.com>
2024-03-04 20:34:36 +0900

    Title: 체크 다이아몬드, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240304/체크 다이아몬드/check-diamond.py
+++ 240304/체크 다이아몬드/check-diamond.py
@@ -1,7 +1,7 @@
 n = int(input())
 
-for i in range(1, 2 * n - 3):
+for i in range(1, 2 * n -n):
     print(" " * (n - i - 1), "* " * i)
 print("* " * n)
-for i in range(2 * n - 4, 0, - 1):
+for i in range(2 * n - 1 - n, 0, - 1):
     print(" " * (n - i - 1), "* " * i)
