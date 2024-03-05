3c2b3733572a1675c7104c63d333747390821a68
yoojin <65722972+Yoojin-An@users.noreply.github.com>
2024-03-05 12:36:03 +0900

    Title: 구구단 만들기 2, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240305/구구단 만들기 2/print-multiplication-table-2.py
@@ -0,0 +1,8 @@
+a, b = map(int, input().split())
+
+for i in range(2, 9, 2):
+    line = ""
+    for j in range(b, a - 1, -1):
+        line += f"{j} * {i} = {i*j} / "
+        print(line[: -2])
+    print()
