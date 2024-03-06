43dc81403a3251949691ed6986164fef04622a91
yoojin <65722972+Yoojin-An@users.noreply.github.com>
2024-03-07 08:49:27 +0900

    Title: 알파벳 출력하기 2, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240307/알파벳 출력하기 2/print-alphabet-2.py
+++ 240307/알파벳 출력하기 2/print-alphabet-2.py
@@ -1,11 +1,14 @@
 n = int(input())
 
 num = ord('A')
+last_num = ord('Z')
 for i in range(n):
     for j in range(n):
         if i <= j:
             print(chr(num), end=" ")
             num += 1
+            if num > last_num:
+                num = ord('A')
         else:
             print(" ", end=" ")
     print()
