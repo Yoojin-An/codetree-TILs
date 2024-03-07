0ff4d708ec5050d0f80ab43af165cf21902a71cb
yoojin <65722972+Yoojin-An@users.noreply.github.com>
2024-03-07 23:23:27 +0900

    Title: 나눗셈의 나머지, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240307/나눗셈의 나머지/remainder-of-division.py
@@ -0,0 +1,20 @@
+a, b = map(int, input().split())
+
+remainders = {}
+answer = 0
+
+while True:
+    remainder = str(a % b)
+    if remainder not in remainders:
+        remainders[remainder] = 0
+    remainders[remainder] += 1
+    a = a // b
+    if a == 0:
+        break
+
+# 나머지가 등장한 횟수만 리스트에 담는다.
+values = remainders.values()
+for i in values:
+    answer += i ** 2 # 등장한 횟수를 제곱해서 더하기
+
+print(answer)
