d51d46d444873a567ffb2962d74330cfe475e40c
yoojin <65722972+Yoojin-An@users.noreply.github.com>
2024-03-17 17:47:36 +0900

    Title: 합을 옆으로 밀어 출력, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240317/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.py
+++ 240317/합을 옆으로 밀어 출력/push-the-sum-sideways-to-output.py
@@ -3,5 +3,5 @@ sum_of_nums = 0
 for _ in range(n):
     sum_of_nums += int(input())
 
-answer = str(sum_of_nums)[1:-1] + str(sum_of_nums)[0]
+answer = str(sum_of_nums)[1:] + str(sum_of_nums)[0]
 print(answer)
