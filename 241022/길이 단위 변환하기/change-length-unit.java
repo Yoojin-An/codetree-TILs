d8e786d758d3daa0dd70dab7d723d8a6ef1a913c
anewjean <65722972+Yoojin-An@users.noreply.github.com>
2024-10-22 21:47:06 +0900

    Title: 길이 단위 변환하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241022/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        double ft = 30.48;
+        int mi = 160934;
+        System.out.printf("9.2ft = f.1%cm\n", ft * 9.2);
+        System.out.printf("1.3mi = f.1%cm\n", mi * 1.3);
+    }
+}
