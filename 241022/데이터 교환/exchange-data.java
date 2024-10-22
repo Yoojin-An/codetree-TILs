ff5d886c6cf9ee013fddfe2c0eb44dec95e01bb3
anewjean <65722972+Yoojin-An@users.noreply.github.com>
2024-10-22 22:00:15 +0900

    Title: 데이터 교환, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241022/데이터 교환/exchange-data.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 5, b = 6, c = 7;
+        int temp1 = a;
+        int temp2 = b;
+        a = c;
+        b = temp1;
+        c = temp2;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
